a384c03c2f1a1d33fc634be77037a546323b0657
Pranav Bhandari <pranavbhandari@Pranavs-MacBook-Pro.local>
2017-04-03 20:55:40 -0400

    created model and added few data

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDel : AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context : NSManagedObjectContext = appDel.persistentContainer.viewContext
+        
+        var newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+    /*    newUser.setValue("Pranav", forKey: "username")
+        newUser.setValue("pranav123", forKey: "password")
+        
+        do{
+            try context.save()
+        } catch{
+            print("Something went wrong")
+        }
+        
+ */
+        
+        //let request = NSFetchRequest(entityName: "Users")
+        let request: NSFetchRequest<NSFetchRequestResult> = Users.fetchRequest()
+
+        request.returnsObjectsAsFaults = false
+        do{
+            let results = try context.fetch(request)
+            print("---",results)
+        } catch{
+            print("something wrong")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
